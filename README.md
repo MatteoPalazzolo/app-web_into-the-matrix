7b3f39d772e4b852014774a2510087cbeb0df58c
Matteo <palamatteocp2004@gmail.com>
2024-06-08 20:02:53 +0200

    working grid layout; home.html almost done;

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # into-the-matrix
+
+### TODO
+- bloccare accesso sito da smartphone
+- creare accessi
+- creare validazione token che porterà al sito
+- impostare homepage
---
+++ php/inventory.php
@@ -0,0 +1,42 @@
+<style>
+    
+    .table-wrapper {
+        border: 3px solid rgb(104, 98, 98);
+        border-radius: 20px;
+        background-color: rgb(194, 194, 194);
+        color: white;
+        margin: 5%;
+    }
+
+    table, td, th {
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+        height: 70px;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        border-style: hidden;
+    }
+
+</style>
+
+<!-- tabella -->
+<div class="table-wrapper">
+    <table>
+        <tr>
+            <td>A</td><td>B</td><td>C</td><td>D</td><td>E</td>
+        </tr>
+        <tr>
+            <td>F</td><td>G</td><td>H</td><td>I</td><td>J</td>
+        </tr>
+        <tr>
+            <td>F</td><td>G</td><td>H</td><td>I</td><td>J</td>
+        </tr>
+        <tr>
+            <td>F</td><td>G</td><td>H</td><td>I</td><td>J</td>
+        </tr>
+    </table>
+</div>
